4bf3868381e2b092ab7deaa0394071627183d91b
ASErashov <ASErashov@IT-ASERASHOV>
2018-04-18 17:05:36 +0300

    initiate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import 'rxjs/add/operator/toPromise';
+import { GitSearchService } from './git-search.service';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GitSearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
